09fdbf84b29be1fbbb9467c56b54f9caa4988a32
tnghk <tnghk9611@naver.com>
2022-01-03 22:55:51 +0900

    수정

--- 1weeks/BOJ_1065_sh.py
+++ 1weeks/BOJ_1065_sh.py
@@ -1,15 +1,6 @@
-# 문제 주요 내용 : 1이상이고, 입력받은 숫자이하인 한수의 갯수 구하기
-# 출력조건 : 갯수를 출력
-
-# 한수의 조건 : 각 자리가 등차수열을 이룬다
-# 고려 사항 : 두 자리 이하 숫자는 모두 한수 취급
-
 def hansu(input_num):
-    # 브루트포스 알고리즘
-    # 1-입력값까지의 수를 생성하여 각자리수별로 리스트 만들기
     count, sub_1, sub_2 = 0, 0, 0
     for i in range(1, input_num + 1):
-         # 100보다 작은수 -> 모두 한수로 count
         if i < 100:
             count += 1
         else:
@@ -20,11 +11,8 @@ def hansu(input_num):
                 count += 1
     return count
 
-
-# 사용자에게 입력받기
 input_num = int(input())
 
-# 예외 처리
 if input_num < 0 or input_num > 1000:
     print("입력 범위를 벗어났습니다.")
     exit()
--- 1weeks/BOJ_2309_sh.py
+++ 1weeks/BOJ_2309_sh.py
@@ -1,26 +1,16 @@
-# 문제 주요 내용 : 9개의 숫자가 주어졌을 때, 그중 7개의 합이 100이되는 조합을 찾는 문제
-# 출력조건 : 오름차순
-
-# 입력숫자 설정
 input_num = 9
-
-# 9번의 입력 받기
 input_list = [int(input()) for i in range(input_num)]
 
 sum_value = sum(input_list)
 temp_1, temp_2 = 0, 0
 
-# 브루트포스 알고리즘
-# 이중포문을 활용하여 전체적으로 검사 진행, 전체 입력값의 합에서 2가지 원소를 제거한 값이 100일때 temp1,temp2에 두개의 값 저장
 for i in range(input_num):
     for j in range(i+1, input_num):
         if (sum_value - (input_list[i] + input_list[j])) == 100:
             temp_1 = input_list[i]
             temp_2 = input_list[j]
 
-# 두개의 원소 삭제
 input_list.remove(temp_1)
 input_list.remove(temp_2)
 
-# 정렬하여 출력
 print('\n'.join(map(str, sorted(input_list))))
