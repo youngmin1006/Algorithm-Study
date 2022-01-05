a4421a5907a414253000ad64946b13407380326b
bibidi <76458724+tnwlsgk1002@users.noreply.github.com>
2022-01-05 19:15:20 +0900

    Update 일곱난쟁이.py

--- 1주차/일곱난쟁이.py
+++ 1주차/일곱난쟁이.py
@@ -17,7 +17,7 @@ for i, age in enumerate(ages) :
     n1 = age
     for j in range(i+1, n) :
         n2 = ages[j]
-        if (total-n1-n2) == 100 :
+        if (total-n1-n2) == 100 : # 두 사람이 일곱 난쟁이가 아닐 경우  
             answer = True
             break
     if answer :
