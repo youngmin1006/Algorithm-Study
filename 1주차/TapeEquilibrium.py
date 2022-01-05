0214c2f41e115dad62966b9047d4d3b68827d1c4
bibidi <76458724+tnwlsgk1002@users.noreply.github.com>
2022-01-05 20:00:07 +0900

    Codility Lesson3 : TapeEquilibrium

--- 1주차/TapeEquilibrium.py
+++ 1주차/TapeEquilibrium.py
@@ -5,10 +5,13 @@ def diff(p1, p2) :
         return p1-p2
 
 def solution(A):
+    # P가 1일 경우의 각 구간의 합과 차이
     part = 1
     p1 = sum(A[:1])
     p2 = sum(A[1:])
     min = diff(p1, p2)
+    
+    # P가 2 이상 일 경우
     for part, a in zip(range(2, len(A)), A[1:]) :
         p1 += a
         p2 -= a
