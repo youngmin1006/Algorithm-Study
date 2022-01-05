06a8a0ce7b16ddb791b9258ccd47aa79e799be42
bibidi <76458724+tnwlsgk1002@users.noreply.github.com>
2022-01-05 19:25:34 +0900

    백준 2309번 : 일곱 난쟁이

--- 1주차/일곱난쟁이.py
+++ 1주차/일곱난쟁이.py
@@ -17,7 +17,7 @@ for i, age in enumerate(ages) :
     n1 = age
     for j in range(i+1, n) :
         n2 = ages[j]
-        if (total-n1-n2) == 100 : # 두 사람이 일곱 난쟁이가 아닐 경우  
+        if (total-n1-n2) == 100 : # 두 사람이 일곱 난쟁이가 아닐 경우(total - 특정 두명 == 100) 
             answer = True
             break
     if answer :
