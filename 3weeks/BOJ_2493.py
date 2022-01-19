03eb67a6227fe6f8fe7373a203a61c679729bcf8
tnghk <tnghk9611@naver.com>
2022-01-19 19:09:07 +0900

    탑

---
+++ 3weeks/BOJ_2493.py
@@ -0,0 +1,21 @@
+# 탑
+# 수신 받는 탑의 인덱스 출력
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+top = list(map(int, input().split()))
+stack = []
+idx = [0 for i in range(n)]
+
+for i in range(len(top)):
+    while stack:
+        if stack[-1][1] > top[i]:
+            idx[i] = stack[-1][0] + 1
+            break
+        else: # 오른쪽 탑의 높이가 더 높은 경우
+            stack.pop()
+    stack.append([i, top[i]]) # 일단 처음엔 [인덱스, 높이] append
+
+print(*idx)
